4ad9ee128c8b819408cb6f6fea586b936ab07db3
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2020-04-17 15:56:02 -0300

    Add parameter to specify amount

--- pipeline.py
+++ pipeline.py
@@ -1,16 +1,16 @@
 import pandas as pd
 
 
-def most_rated(frame):
-    most_rated_id = frame.rating_count_tot.idxmax()
+def most_rated(frame, n=1):
+    most_rated_id = frame.rating_count_tot.nlargest(n).index
     return frame.loc[most_rated_id]
 
 
 data = pd.read_csv('data/AppleStore.csv')
 news = data[data.prime_genre == 'News']
-print(most_rated(news).track_name)
+print(most_rated(news).track_name.values[0])
 
 book = data[data.prime_genre == 'Book']
 music = data[data.prime_genre == 'Music']
-print(most_rated(book).track_name)
-print(most_rated(music).track_name)
+print(most_rated(book, 10).track_name.values)
+print(most_rated(music, 10).track_name.values)
