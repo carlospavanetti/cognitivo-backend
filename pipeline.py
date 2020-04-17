bbc921326fff18921484e0ebcfcd2c5f13596fb5
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2020-04-17 15:14:38 -0300

    Extract most_rated and apply it to book and music

--- pipeline.py
+++ pipeline.py
@@ -1,7 +1,16 @@
 import pandas as pd
 
+
+def most_rated(frame):
+    most_rated_id = frame.rating_count_tot.idxmax()
+    return frame.loc[most_rated_id]
+
+
 data = pd.read_csv('data/AppleStore.csv')
 news = data[data.prime_genre == 'News']
-most_rated_id = news.rating_count_tot.idxmax()
-most_rated = news.loc[most_rated_id]
-print(most_rated.track_name)
+print(most_rated(news).track_name)
+
+book = data[data.prime_genre == 'Book']
+music = data[data.prime_genre == 'Music']
+print(most_rated(book).track_name)
+print(most_rated(music).track_name)
