8627835eabdf7a8c4e9c70ff4bb867a9480deb8f
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2020-04-18 13:31:44 -0300

    Extract top book & music apps method

--- pipeline.py
+++ pipeline.py
@@ -8,15 +8,15 @@ def most_rated(frame, n=1):
     return frame.loc[most_rated_id]
 
 
+def top_book_and_music_apps(frame):
+    book_apps = frame[frame.prime_genre == 'Book']
+    music_apps = frame[frame.prime_genre == 'Music']
+    top_book = most_rated(book_apps, 10)
+    top_music = most_rated(music_apps, 10)
+    return pd.concat([top_book, top_music])
+
+
 data = pd.read_csv('data/AppleStore.csv')
 news = data[data.prime_genre == 'News']
 print(most_rated(news).track_name.values[0])
-
-book = data[data.prime_genre == 'Book']
-music = data[data.prime_genre == 'Music']
-print(most_rated(book, 10).track_name.values)
-print(most_rated(music, 10).track_name.values)
-
-f = open('out.json', 'w')
-load_dotenv()
-s = client().search.tweets(q="pycon", count=100)
+selected_apps = top_book_and_music_apps(data)
