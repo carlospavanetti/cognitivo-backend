a12f966d974bef12fb5d1542b41c9eca08ccf9b0
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2020-04-18 22:39:41 -0300

    Update pipeline with tweets citations enrichment

--- pipeline.py
+++ pipeline.py
@@ -1,6 +1,6 @@
+from time import time
 import pandas as pd
-from dotenv import load_dotenv
-from services.twitter import client
+from services.tweets_count import TweetsCount
 
 
 def most_rated(frame, n=1):
@@ -16,7 +16,35 @@ def top_book_and_music_apps(frame):
     return pd.concat([top_book, top_music])
 
 
+def preprocessed_name(name):
+    # clean_name = re.sub(r'(-|–|&|,|!|and|by)', '', name)
+    limited_name = ' '.join(name.split()[:4])
+    return limited_name
+
+
+def tweets_citations(query, id=0):
+    if id == 82:
+        return TweetsCount(query).value()
+    return 0
+
+
+def with_tweets_citations_count(frame):
+    citations = [tweets_citations(preprocessed_name(
+        app.track_name), i) for i, app in frame.iterrows()]
+    frame['n_citacoes'] = citations
+    return frame
+
+
 data = pd.read_csv('data/AppleStore.csv')
 news = data[data.prime_genre == 'News']
 print(most_rated(news).track_name.values[0])
 selected_apps = top_book_and_music_apps(data)
+enriched_data = with_tweets_citations_count(selected_apps)
+output_data = enriched_data[
+    ['id', 'track_name', 'n_citacoes', 'size_bytes', 'price', 'prime_genre']
+]
+
+timestamp = int(time())
+filename = '{timestamp}_output'.format(timestamp=timestamp)
+output_data.to_csv('{name}.csv'.format(name=filename), index=False)
+output_data.to_json('{name}.json'.format(name=filename), orient='records')
