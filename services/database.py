87a2b0a69a7caca1258ab69148fbf0a0242f6349
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2020-04-18 23:05:52 -0300

    Add sqlite support

---
+++ services/database.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine
+
+engine = create_engine('sqlite:///output/database.db')
+
+
+def save_dataframe(frame, table, timestamp):
+    frame['saved_at'] = [timestamp] * len(frame.index)
+    frame.to_sql(table, con=engine)
