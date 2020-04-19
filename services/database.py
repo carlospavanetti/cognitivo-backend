075992c36c7543081b27d3af30f46d16892546b9
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2020-04-18 23:11:08 -0300

    Update database service to append in table

--- services/database.py
+++ services/database.py
@@ -5,4 +5,4 @@ engine = create_engine('sqlite:///output/database.db')
 
 def save_dataframe(frame, table, timestamp):
     frame['saved_at'] = [timestamp] * len(frame.index)
-    frame.to_sql(table, con=engine)
+    frame.to_sql(table, con=engine, if_exists='append')
